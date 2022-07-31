eb0093802f91814623b071a793891b410a2a0012
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-01 01:18:19 +0530

    student schema created

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require("express");
 const app = express();
 const studentRoute = require("./api/routes/student");
 const facultyRoute = require("./api/routes/faculty");
+
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://Imrihan:Imrihan@mrk.0vudv.mongodb.net/?retryWrites=true&w=majority"
+);
+
+mongoose.connection.on("error", (error) => {
+  console.log("connection failed");
+});
+
+mongoose.connection.on("connected", (connected) => {
+  console.log("connected with database...");
+});
+
 app.use("/student", studentRoute);
 app.use("/faculty", facultyRoute);
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: "app is running in localhost: 3000",
+  res.status(404).json({
+    message: "bad request",
   });
 });
 
