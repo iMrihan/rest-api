2b113bdd356f9b8abc3c06824c13e64dadd181cd
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-15 18:56:35 +0530

    post route is added

--- api/routes/student.js
+++ api/routes/student.js
@@ -2,11 +2,39 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../model/student");
 const mongoose = require("mongoose");
+const student = require("../model/student");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "This is student get request",
-  });
+  Student.find()
+    .then((result) => {
+      res.status(200).json({
+        studentData: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  console.log(req.params.id);
+
+  student
+    .findById(req.params.id)
+    .then((result) => {
+      res.status(200).json({
+        student: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/", (req, res, next) => {
