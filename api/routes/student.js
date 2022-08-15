3b08143de88483678526185d8b02726023dfbe68
iMrihan <mhd.khan.rihan@gmail.com>
2022-08-15 19:15:01 +0530

    delete route is added

--- api/routes/student.js
+++ api/routes/student.js
@@ -76,4 +76,28 @@ router.delete("/:id", (req, res, next) => {
     });
 });
 
+router.put("/:id", (req, res, next) => {
+  Student.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        gender: req.body.gender,
+      },
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        update_product: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
